d73eb9fed3a2a49cb29b5f50825dad69dce1971c
Liyana kp <liyana.kp8590@gmail.com>
2024-11-19 00:15:07 +0530

    feat: do hooks

---
+++ src/component/MyButton.jsx
@@ -0,0 +1,113 @@
+//create a button 
+
+
+
+// export const MyButton = ()=>{
+
+//    return(
+// <div>
+//     <h1>hello</h1>
+//     <button>CLICK ME</button>
+// </div>
+//    )
+// }
+
+
+
+
+// const product = [
+//     {item:'mango',isFriut:true,id:'1'},
+//     {item:'carrot',isFriut:false,id:'2'}
+
+// ]
+
+
+// export const ListItems =()=>{
+  
+        
+//             const allProduct = product.map((produucts)=>(
+
+//                 <li
+//                 key={produucts.id}
+//                 style={{
+//                     color:produucts.isFriut?'green':'red',
+//                 }}>
+//                    {produucts.item}
+//                 </li>
+//             ))
+//             return(
+//                 <ul>{allProduct}</ul>
+//             )
+
+   
+// }
+
+
+// export const MyButton =()=>{
+//    function handleClick(){
+//         alert('you clicked me!')
+//     }
+
+//     return(
+//         <button onClick={handleClick}>CLICK</button>
+//     )
+// }
+
+
+
+import { useState } from "react";
+
+
+const image={
+    imageUrl:'https://images.pexels.com/photos/143133/pexels-photo-143133.jpeg',
+
+}
+
+
+export const MyButton =()=>{
+
+
+
+    const [count,setCount]=useState(0);
+
+    function handleIncrement(){
+        setCount(count + 1)
+    }
+    const [countMinus,setcountMinus]=useState(0);
+
+    function handleDecrement(){
+        setcountMinus(count - 1)
+    }
+
+    return(
+       <div>
+
+        <img
+        src={image.imageUrl}
+        alt="carrot"
+        style={
+            {
+                width:200,
+                height:200,
+                paddingTop:30,
+                borderRadius:10, 
+            
+
+            }
+        }
+        
+        ></img>
+
+         <div style={{display:"flex",justifyContent:"center"}}>
+        <button onClick={handleIncrement} style={{}}> +{count} </button>
+        <button onClick={handleDecrement} style={{}}>-{countMinus} </button>
+   
+        </div>
+        
+
+       </div>
+        
+       
+
+    )
+}
